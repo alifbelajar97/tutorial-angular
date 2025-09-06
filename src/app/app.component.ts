9c7d4678b0b635f4990675bd11c460cb9e63e36d
Alif Razan Saputra <alifbelajar97@gmail.com>
2025-09-06 16:06:20 +0700

    input exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
 import { DUMMY_USERS } from './mocks/dummy-user';
+import { TaskComponent } from './components/task/task.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    TaskComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find(user => user.id == this.selectedUserId)!;
+  }
+
   onSelect(id: any){
-    console.log('ID', id)
+    this.selectedUserId = id;
   }
 }
