98a5159a5d97bdfae685718dc574d2695c59f7cc
Alif Razan Saputra <alifbelajar97@gmail.com>
2025-09-06 14:37:10 +0700

    output

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, output } from '@angular/core';
 import { DUMMY_USERS } from '../../mocks/dummy-user';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -13,7 +13,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return 'assets/images/' + this.avatar
@@ -21,5 +22,5 @@ export class UserComponent {
 
   onSelectUser() {
     this.select.emit(this.id);
-   }
+  }
 }
