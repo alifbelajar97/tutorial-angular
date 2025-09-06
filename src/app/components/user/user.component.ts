3206232540895677d309f801f5f6726b75b00a97
Alif Razan Saputra <alifbelajar97@gmail.com>
2025-09-06 12:05:38 +0700

    penggunaan signal - woow keren, dengan menggunakan signal perubahan akan hanya terjadi di component itu saja tanpa perlu melakukan check rendering ke seluruh app seperti yang dilakukan zone.js (default angular versi < 16)

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../../mocks/dummy-user';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]) 
+  imagePath = computed(() =>  'assets/images/' + this.selectedUser().avatar)
+
+  // get imagePath() {
+  //   return 'assets/images/' + this.selectedUser.avatar
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+}
