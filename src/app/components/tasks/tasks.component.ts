ddcea965be9baf7ecf2ab8e392a7a4d688d1c4fa
Alif Razan Saputra <alifbelajar97@gmail.com>
2025-09-10 16:06:33 +0700

    45

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 
 @Component({
@@ -9,7 +9,39 @@ import { TaskComponent } from './task/task.component';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnChanges{
   @Input({ required: true }) name!: any;
+  @Input({ required: true }) userId!: string;
 
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master ANgular',
+      summary: 'Lorem Ipsum set dolor amet',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master ANgular',
+      summary: 'Lorem Ipsum set dolor amet',
+      dueDate: '2025-12-31'
+    },
+     {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master ANgular',
+      summary: 'Lorem Ipsum set dolor amet',
+      dueDate: '2025-12-31'
+    },
+  ]
+
+  get selectUserTask(){
+    return this.tasks.filter((task) => task.userId === this.userId)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changeschanges', changes)
+  }
 }
